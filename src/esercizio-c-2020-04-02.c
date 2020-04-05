b71a011234a696e3c3123ed37a2f876fb119705c
AndreaGonzato <gonzatoandrea98@gmail.com>
2020-04-05 18:43:35 +0200

    41/5000

--- src/esercizio-c-2020-04-02.c
+++ src/esercizio-c-2020-04-02.c
@@ -4,7 +4,12 @@
 #include <string.h>
 #include <sys/wait.h>
 
-#define NUM_CHARS  80
+#define NUM_CHARS  8
+
+unsigned char get_lowest_char(char * char_array);
+unsigned char get_higher_char(char * char_array);
+unsigned char get_most_used_char(char * char_array);
+unsigned char get_most_unusual_char(char * char_array);
 
 int main(int argc, char *argv[]) {
 
@@ -32,70 +37,12 @@ int main(int argc, char *argv[]) {
 			if ( child_pid == 0){
 				// child
 
-				unsigned char lowest_char = 255;
-				unsigned char higher_char = 0;
+				unsigned char lowest_char = get_lowest_char(char_array);
+				unsigned char higher_char = get_higher_char(char_array);
 
-				unsigned char most_used_char = char_array[0];
-				short int repetition_of_the_most_used_char = 1;
-				unsigned char most_unusual_char = char_array[0];
-				short int repetitions_of_the_most_unusual_char = NUM_CHARS;
+				unsigned char most_used_char = get_most_used_char(char_array);
+				unsigned char most_unusual_char = get_most_unusual_char(char_array);
 
-				/*
-				printf("[child] print all elements of char_array that I recived:\n");
-				for(int i=0 ; i<NUM_CHARS ; i++){
-					printf("%d ", char_array[i]);
-				}
-				printf("\n");
-				*/
-
-				for ( int i = 0 ; i < NUM_CHARS ; i++){
-					// determinates lowest char
-					if (char_array[i] < lowest_char){
-						lowest_char = char_array[i];
-					}
-					// determinates higher char
-					if (char_array[i] > higher_char){
-						higher_char = char_array[i];
-					}
-				}
-
-				/*
-				 * This block of code is useful only if char_array has some NULL values
-				 * that are the most_used_char/most_unusual_char/lowest_char/higher_char
-				*/
-				short int count_null=0;
-				for (int i=0 ; i < NUM_CHARS ; i++){
-					if(char_array[i] == NULL){
-						count_null++;
-					}
-				}
-				if(count_null >= 1){
-					most_used_char = NULL;
-					repetition_of_the_most_used_char = count_null;
-					most_unusual_char = NULL;
-					repetitions_of_the_most_unusual_char = count_null;
-				}
-
-				// determinate most unusual char & most usual char
-				for (int i=0 ; i < NUM_CHARS ; i++){
-					if (char_array[i] != NULL){
-						short int repetitions = 1;
-						for (int j = i+1 ; j < NUM_CHARS ; j++){
-							if (char_array[i] == char_array[j]){
-								repetitions++;
-								char_array[j] = NULL;
-							}
-						}
-						if (repetitions > repetition_of_the_most_used_char){
-							repetition_of_the_most_used_char = repetitions;
-							most_used_char = char_array[i];
-						}
-						if (repetitions < repetitions_of_the_most_unusual_char){
-							repetitions_of_the_most_unusual_char = repetitions;
-							most_unusual_char = char_array[i];
-						}
-					}
-				}
 
 				printf("\n[child] lowest char: %c\t\t\tin ascii code is: %d\n",lowest_char, lowest_char);
 				printf("[child] higher char: %c\t\t\tin ascii code is: %d \n",higher_char, higher_char);
@@ -114,7 +61,6 @@ int main(int argc, char *argv[]) {
 				char_array = calloc(NUM_CHARS, sizeof(char));
 				numbers_of_childs++;
 				//printf("[parent] waiting for my child process to end\n");
-				c = 10;
 				if (wait(NULL) == -1) {
 					perror("wait error");
 				} else {
@@ -130,4 +76,94 @@ int main(int argc, char *argv[]) {
 	printf("\n[father] I threw %d proces\n", numbers_of_childs);
 
 	return 0;
+}
+
+unsigned char get_lowest_char(char * char_array){
+	unsigned char lowest_char = 255;
+	for ( int i = 0 ; i < NUM_CHARS ; i++){
+		// determinates lowest char
+		if (char_array[i] < lowest_char){
+			lowest_char = char_array[i];
+		}
+	}
+	return lowest_char;
+}
+
+unsigned char get_higher_char(char * char_array){
+	unsigned char higher_char = 0;
+	for ( int i = 0 ; i < NUM_CHARS ; i++){
+		if (char_array[i] > higher_char){
+			higher_char = char_array[i];
+		}
+	}
+	return higher_char;
+}
+
+unsigned char get_most_used_char(char * char_array){
+	unsigned char most_used_char = NULL;
+
+	short int count_null = 0;
+	for (int i=0 ; i < NUM_CHARS ; i++){
+		if(char_array[i] == NULL){
+			count_null++;
+		}
+	}
+
+	short int repetition_of_the_most_used_char = 0;
+
+	for (int i=0 ; i < NUM_CHARS ; i++){
+		short int repetitions = 1;
+		for (int j = i+1 ; j < NUM_CHARS ; j++){
+			if (char_array[i] == char_array[j]){
+				repetitions++;
+			}
+		}
+		if (repetitions > repetition_of_the_most_used_char){
+			repetition_of_the_most_used_char = repetitions;
+			most_used_char = char_array[i];
+		}
+	}
+
+	if(repetition_of_the_most_used_char > count_null){
+		return most_used_char;
+	}else{
+		return 0;
+	}
+}
+
+unsigned char get_most_unusual_char(char * char_array){
+	unsigned char most_unusual_char = char_array[0];
+
+	short int count_null = 0;
+	for (int i=0 ; i < NUM_CHARS ; i++){
+		if(char_array[i] == NULL){
+			count_null++;
+		}
+	}
+
+	short int repetition_of_the_most_unusual_char = NUM_CHARS;
+
+	for (int i=0 ; i < NUM_CHARS ; i++){
+		if (char_array[i] != NULL){
+			short int repetitions = 1;
+			for (int j = i+1 ; j < NUM_CHARS ; j++){
+				if (char_array[i] == char_array[j]){
+					repetitions++;
+					char_array[j] = NULL;
+				}
+			}
+			if (repetitions < repetition_of_the_most_unusual_char){
+				repetition_of_the_most_unusual_char = repetitions;
+				most_unusual_char = char_array[i];
+				//printf("most_unusual_char: %c\n",most_unusual_char);
+				//printf("repetition_of_the_most_unusual_char: %hi\n",repetition_of_the_most_unusual_char);
+			}
+		}
+	}
+
+	if(count_null != 0 && count_null < repetition_of_the_most_unusual_char ){
+		return 0;
+	}else{
+		return most_unusual_char;
+	}
 }
